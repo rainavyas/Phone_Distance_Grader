814666dbbb551c03a6b739a13fd6fe0ab6090fb2
rainavyas <sayvaniar@gmail.com>
2020-10-06 12:00:41 +0100

    pickle file open

--- training.py
+++ training.py
@@ -2,3 +2,7 @@ import torch
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 from models import FCC
+
+
+input_file = "BLXXXgrd02.pkl"
+pkl = pickle.load(open(input_file, "rb"))
