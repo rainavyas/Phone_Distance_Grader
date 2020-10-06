95f4463f2618f8e48b885a636bbd9ee0c48d45db
rainavyas <sayvaniar@gmail.com>
2020-10-06 17:08:31 +0100

    batch norm

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
-import time
+
 class FCC(torch.nn.Module):
     def __init__(self, num_features):
 
         super(FCC, self).__init__()
+        self.bn1 = torch.nn.BatchNorm1d(num_features=num_features)
         self.layer1 = torch.nn.Linear(num_features, 500)
         self.layer2 = torch.nn.Linear(500, 200)
         self.layer3 = torch.nn.Linear(200, 10)
@@ -12,13 +13,15 @@ class FCC(torch.nn.Module):
 
     def forward(self, X):
 
-        # Check input is nan
-        #nan_list = torch.isnan(torch.sum(X, 1)).tolist()
-        #if True in nan_list:
-        #    print("some values from input are nan")
-        time.sleep(2)
-        h1 = F.relu(self.layer1(X))
-        print(h1[0][:10])
+        # Check if input has nan
+        nan_list = torch.isnan(torch.sum(X, 1)).tolist()
+        if True in nan_list:
+           print("some values from input are nan")
+
+        # Normalize each input vector
+        X_norm = self.bn1(X)
+
+        h1 = F.relu(self.layer1(X_norm))
         h2 = F.relu(self.layer2(h1))
         h3 = F.relu(self.layer3(h2))
         y = self.layer4(h3)
